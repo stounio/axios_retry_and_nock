eabffd5ee47e8ce89585d776e30daf521920c3c2
Guillaume Pichot <guillaume.pichot@auctane.com>
2022-08-30 15:07:59 +0100

    Added a default number of retries when the world experiences difficulties to greet a request

--- src/hello_world_client.js
+++ src/hello_world_client.js
@@ -1,11 +1,23 @@
-const axios = require('axios');
+const rax = require("retry-axios");
+const axios = require("axios");
+
+rax.attach();
+
+const numberOfRetries = process.env.NUMBER_OF_RETRIES || 3;
 
 const greetings = async () => {
   const config = {
     timeout: 100,
-    headers: { 'User-Agent': 'NewbieToAxiosAndNock' },
+    headers: { "User-Agent": "NewbieToAxiosAndNock" },
+    raxConfig: {
+      retry: numberOfRetries,
+      noResponseRetries: 0,
+      onRetryAttempt: (error) => {
+        console.error(`[HELLO-WORLD-RETRY] Retrying request to Hello World\n${error}`);
+      },
+    },
   };
-  return axios.get(`${process.env.HELLO_WORLD_DOMAIN}/greetings`, config)
+  return axios.get(`${process.env.HELLO_WORLD_DOMAIN}/greetings`, config);
 };
 
 module.exports = { greetings };
